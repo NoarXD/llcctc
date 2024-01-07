9224df7d3d5838b9372db64e61395bb3a4331cf2
NoarXD <johnnydpx0@gmail.com>
2024-01-07 11:33:35 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,173 @@
-import Image from 'next/image'
+import Image from "next/image";
+import logollcctc from "../public/logollcctc.jpg";
+import personImg from "../public/person.png";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main className="xl:max-w-screen-lg md:max-w-screen-sm md:mx-auto mx-10">
+            <div>
+                <Image
+                    src={logollcctc}
+                    width={140}
+                    height={140}
+                    className="mx-auto"
+                />
+                <p className="text-center mt-5 font-bold text-xl">
+                    LLCC Toastmaster Club
+                </p>
+            </div>
+            <div className="mt-2">
+                <p className="text-center text-gray-300">
+                    LLCC Toastmaster Club was established on 12/1/2023, led by
+                    Mr Peter, with a total of 21 members.
+                </p>
+            </div>
+            <div className="mt-10 grid grid-cols-10 md:grid-cols-3 gap-0 content-center">
+                <hr className="relative top-[12px] border" />
+                <p className="text-center col-span-8 md:col-auto border-l-2 border-r-2 rounded-md">
+                    LLCC Toastmaster Club Structure
+                </p>
+                <hr className="relative top-[12px] border" />
+            </div>
+            <div className="mt-10">
+                <div className="w-32 mx-auto">
+                    <p className="text-center font-bold">Leader</p>
+                    <Image src={personImg} className="w-32 mt-5" />
+                    <p className="mt-4 text-center">Mr Johnny</p>
+                </div>
+                <div className="grid grid-cols-2">
+                    <div className="w-auto mx-auto">
+                        <p className="text-center font-bold">Vice Leader</p>
+                        <Image src={personImg} className="w-32 mt-5" />
+                        <p className="mt-4 text-center">Mr Khamthong</p>
+                    </div>
+                    <div className="w-auto mx-auto">
+                        <p className="text-center font-bold">Vice Leader</p>
+                        <Image src={personImg} className="w-32 mt-5" />
+                        <p className="mt-4 text-center">Miss Manida</p>
+                    </div>
+                </div>
+                <div className="mt-10 grid grid-cols-5 md:grid-cols-3 gap-0 content-center">
+                    <hr className="relative top-[12px] border" />
+                    <p className="text-center col-span-3 md:col-auto border-l-2 border-r-2 rounded-md">
+                        Department of Activity
+                    </p>
+                    <hr className="relative top-[12px] border" />
+                </div>
+                <div className="mt-10">
+                    <div className="w-32 mx-auto">
+                        <p className="text-center font-bold">Leader</p>
+                        <Image src={personImg} className="w-32 mt-5" />
+                        <p className="mt-4 text-center">Mr Khammoun</p>
+                    </div>
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Tonkar</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Khamxay</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Miss Nouphaluck</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10 grid grid-cols-5 md:grid-cols-3 gap-0 content-center">
+                    <hr className="relative top-[12px] border" />
+                    <p className="text-center col-span-3 md:col-auto border-l-2 border-r-2 rounded-md">
+                        Department of English Speaking
+                    </p>
+                    <hr className="relative top-[12px] border" />
+                </div>
+                <div className="mt-10">
+                    <div className="w-32 mx-auto">
+                        <p className="text-center font-bold">Leader</p>
+                        <Image src={personImg} className="w-32 mt-5" />
+                        <p className="mt-4 text-center">Miss Anna</p>
+                    </div>
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Miss Panda</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Lidsamai</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Miss Pinky</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10 grid grid-cols-5 md:grid-cols-3 gap-0 content-center">
+                    <hr className="relative top-[12px] border" />
+                    <p className="text-center col-span-3 md:col-auto border-l-2 border-r-2 rounded-md">
+                        Department of English Teaching
+                    </p>
+                    <hr className="relative top-[12px] border" />
+                </div>
+                <div className="mt-10">
+                    <div className="w-32 mx-auto">
+                        <p className="text-center font-bold">Leader</p>
+                        <Image src={personImg} className="w-32 mt-5" />
+                        <p className="mt-4 text-center">Mr Sonsay</p>
+                    </div>
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Saysamone</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Yang Kong</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Dala</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Ded</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Sayson</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10 grid grid-cols-5 md:grid-cols-3 gap-0 content-center">
+                    <hr className="relative top-[12px] border" />
+                    <p className="text-center col-span-3 md:col-auto border-l-2 border-r-2 rounded-md">
+                        Department of External relations
+                    </p>
+                    <hr className="relative top-[12px] border" />
+                </div>
+                <div className="mt-10">
+                    <div className="w-32 mx-auto">
+                        <p className="text-center font-bold">Leader</p>
+                        <Image src={personImg} className="w-32 mt-5" />
+                        <p className="mt-4 text-center">Miss Nuttaly</p>
+                    </div>
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Miss Namwarn</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Miss Ulin</p>
+                        </div>
+                        <div className="w-auto mx-auto">
+                            <Image src={personImg} className="w-32 mt-5" />
+                            <p className="mt-4 text-center">Mr Alino</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
---
+++ components/Navbar.js
@@ -0,0 +1,69 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import logollcctc from '../public/logollcctc.jpg'
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    Button,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+} from "@nextui-org/react";
+
+export default function MyNavbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const menuItem = ["Home", "Announcement", "TCTalk", "Activity"];
+    return (
+        <Navbar onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent>
+                <NavbarMenuToggle
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    className="sm:hidden"
+                />
+                <NavbarBrand>
+                    <Link href="/" className="font-bold text-white">LLCC Toastmaster Club</Link>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent justify="end" className="hidden md:inline-flex">
+                <NavbarItem>
+                    <Link color="foreground" href="/">
+                        Home
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="/Announcement">
+                        Announcement
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="TCTalk">
+                        TCTalk
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="Activity">
+                        Activity
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarMenu>
+                {menuItem.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            className="w-full text-white"
+                            href={item === "Home" ? "/" : item}
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+    );
+}
